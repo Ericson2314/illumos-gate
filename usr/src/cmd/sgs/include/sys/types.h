9d12a587dc2ac262cf14e4697b6d079a38474497
John Ericson <John.Ericson@Obsidian.Systems>
2025-01-12 18:19:46 -0500

    Hacky copy files for mcs
    Eventually this will become "compat" library like netbsd and freebsd
    have, allowing for building illumos software on other OSes with
    different libcs.

---
+++ usr/src/cmd/sgs/include/sys/types.h
@@ -0,0 +1,9 @@
+#include <sys/stddef.h>
+
+/*
+ * POSIX Extensions
+ */
+typedef unsigned char   uchar_t;
+typedef unsigned short  ushort_t;
+typedef unsigned int    uint_t;
+typedef unsigned long   ulong_t;
